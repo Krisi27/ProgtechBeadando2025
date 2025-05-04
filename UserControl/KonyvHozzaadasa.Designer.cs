57a4a2b938066f1512e93af3071c1323403a6815
Krisi27 <kisskristof45@gmail.com>
2025-05-04 10:26:28 +0200

    Create KonyvHozzaadasa.Designer.cs

---
+++ UserControl/KonyvHozzaadasa.Designer.cs
@@ -0,0 +1,348 @@
+namespace Konyvtar_Rendszer_Kezeles
+{
+    partial class konyHozzaadasaAblak
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel2 = new Panel();
+            tableLayoutPanel1 = new TableLayoutPanel();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            label9 = new Label();
+            label10 = new Label();
+            dataGridView1 = new DataGridView();
+            label1 = new Label();
+            panel1 = new Panel();
+            comboBox1 = new ComboBox();
+            label5 = new Label();
+            dateTimePicker1 = new DateTimePicker();
+            label2 = new Label();
+            torles = new Button();
+            hozzaad = new Button();
+            textBox2 = new TextBox();
+            label4 = new Label();
+            textBox1 = new TextBox();
+            label3 = new Label();
+            panel2.SuspendLayout();
+            tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel2
+            // 
+            panel2.BackColor = SystemColors.ButtonHighlight;
+            panel2.BorderStyle = BorderStyle.FixedSingle;
+            panel2.Controls.Add(tableLayoutPanel1);
+            panel2.Controls.Add(dataGridView1);
+            panel2.Controls.Add(label1);
+            panel2.Location = new Point(301, 39);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(565, 486);
+            panel2.TabIndex = 3;
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.ColumnCount = 6;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 7.9831934F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 21.4285717F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 26.05042F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 21.2616825F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 23.3644867F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 51F));
+            tableLayoutPanel1.Controls.Add(label6, 0, 0);
+            tableLayoutPanel1.Controls.Add(label7, 1, 0);
+            tableLayoutPanel1.Controls.Add(label8, 2, 0);
+            tableLayoutPanel1.Controls.Add(label9, 3, 0);
+            tableLayoutPanel1.Controls.Add(label10, 4, 0);
+            tableLayoutPanel1.Location = new Point(16, 60);
+            tableLayoutPanel1.Margin = new Padding(3, 4, 3, 4);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 15;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 40F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 27F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 27F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 27F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 27F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 27F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 27F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 27F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 27F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 27F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 27F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 27F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 27F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 27F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 27F));
+            tableLayoutPanel1.Size = new Size(530, 409);
+            tableLayoutPanel1.TabIndex = 2;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Dock = DockStyle.Fill;
+            label6.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 238);
+            label6.Location = new Point(3, 0);
+            label6.Name = "label6";
+            label6.Size = new Size(32, 40);
+            label6.TabIndex = 0;
+            label6.Text = "ID";
+            label6.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Dock = DockStyle.Fill;
+            label7.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 238);
+            label7.Location = new Point(41, 0);
+            label7.Name = "label7";
+            label7.Size = new Size(96, 40);
+            label7.TabIndex = 1;
+            label7.Text = "Köny címe";
+            label7.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label8
+            // 
+            label8.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            label8.AutoSize = true;
+            label8.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 238);
+            label8.Location = new Point(143, 0);
+            label8.Name = "label8";
+            label8.Size = new Size(118, 40);
+            label8.TabIndex = 2;
+            label8.Text = "Szerző";
+            label8.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Dock = DockStyle.Fill;
+            label9.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 238);
+            label9.Location = new Point(267, 0);
+            label9.Name = "label9";
+            label9.Size = new Size(95, 40);
+            label9.TabIndex = 3;
+            label9.Text = "Kiadás dátuma";
+            label9.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Dock = DockStyle.Fill;
+            label10.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 238);
+            label10.Location = new Point(368, 0);
+            label10.Name = "label10";
+            label10.Size = new Size(105, 40);
+            label10.TabIndex = 4;
+            label10.Text = "Státusz";
+            label10.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(16, 59);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(530, 411);
+            dataGridView1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Tahoma", 12F, FontStyle.Regular, GraphicsUnit.Point, 238);
+            label1.Location = new Point(16, 16);
+            label1.Name = "label1";
+            label1.Size = new Size(221, 24);
+            label1.TabIndex = 0;
+            label1.Text = "Az összes kiadott könyv";
+            // 
+            // panel1
+            // 
+            panel1.BackColor = SystemColors.ButtonHighlight;
+            panel1.BorderStyle = BorderStyle.FixedSingle;
+            panel1.Controls.Add(comboBox1);
+            panel1.Controls.Add(label5);
+            panel1.Controls.Add(dateTimePicker1);
+            panel1.Controls.Add(label2);
+            panel1.Controls.Add(torles);
+            panel1.Controls.Add(hozzaad);
+            panel1.Controls.Add(textBox2);
+            panel1.Controls.Add(label4);
+            panel1.Controls.Add(textBox1);
+            panel1.Controls.Add(label3);
+            panel1.Location = new Point(15, 39);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(265, 486);
+            panel1.TabIndex = 2;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "Elérhető", "Nem elérhető" });
+            comboBox1.Location = new Point(85, 229);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(170, 28);
+            comboBox1.TabIndex = 19;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Tahoma", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 238);
+            label5.Location = new Point(7, 231);
+            label5.Name = "label5";
+            label5.Size = new Size(71, 21);
+            label5.TabIndex = 18;
+            label5.Text = "Státusz:";
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Location = new Point(7, 188);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(247, 27);
+            dateTimePicker1.TabIndex = 17;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Tahoma", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 238);
+            label2.Location = new Point(5, 155);
+            label2.Name = "label2";
+            label2.Size = new Size(126, 21);
+            label2.TabIndex = 16;
+            label2.Text = "Kiadás dátuma:";
+            // 
+            // torles
+            // 
+            torles.BackColor = Color.FromArgb(14, 128, 87);
+            torles.FlatAppearance.BorderSize = 0;
+            torles.FlatAppearance.CheckedBackColor = Color.DarkSeaGreen;
+            torles.FlatAppearance.MouseDownBackColor = Color.DarkSeaGreen;
+            torles.FlatStyle = FlatStyle.Flat;
+            torles.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 238);
+            torles.ForeColor = SystemColors.ButtonHighlight;
+            torles.Location = new Point(137, 309);
+            torles.Name = "torles";
+            torles.Size = new Size(114, 59);
+            torles.TabIndex = 15;
+            torles.Text = "Törlés";
+            torles.UseVisualStyleBackColor = false;
+            torles.Click += torles_Click;
+            // 
+            // hozzaad
+            // 
+            hozzaad.BackColor = Color.FromArgb(14, 128, 87);
+            hozzaad.FlatAppearance.BorderSize = 0;
+            hozzaad.FlatAppearance.CheckedBackColor = Color.DarkSeaGreen;
+            hozzaad.FlatAppearance.MouseDownBackColor = Color.DarkSeaGreen;
+            hozzaad.FlatStyle = FlatStyle.Flat;
+            hozzaad.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 238);
+            hozzaad.ForeColor = SystemColors.ButtonHighlight;
+            hozzaad.Location = new Point(17, 309);
+            hozzaad.Name = "hozzaad";
+            hozzaad.Size = new Size(94, 59);
+            hozzaad.TabIndex = 14;
+            hozzaad.Text = "Hozzáad";
+            hozzaad.UseVisualStyleBackColor = false;
+            hozzaad.Click += hozzaad_Click;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(75, 99);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(179, 27);
+            textBox2.TabIndex = 11;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Tahoma", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 238);
+            label4.Location = new Point(8, 100);
+            label4.Name = "label4";
+            label4.Size = new Size(65, 21);
+            label4.TabIndex = 10;
+            label4.Text = "Szerző:";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(102, 59);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(153, 27);
+            textBox1.TabIndex = 9;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Tahoma", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 238);
+            label3.Location = new Point(7, 60);
+            label3.Name = "label3";
+            label3.Size = new Size(100, 21);
+            label3.TabIndex = 8;
+            label3.Text = "Könyv címe:";
+            // 
+            // konyHozzaadasaAblak
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            Name = "konyHozzaadasaAblak";
+            Size = new Size(880, 565);
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            tableLayoutPanel1.ResumeLayout(false);
+            tableLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel2;
+        private DataGridView dataGridView1;
+        private Label label1;
+        private Panel panel1;
+        private Button torles;
+        private Button hozzaad;
+        private Label label2;
+        private TextBox textBox2;
+        private Label label4;
+        private TextBox textBox1;
+        private Label label3;
+        private ComboBox comboBox1;
+        private Label label5;
+        private DateTimePicker dateTimePicker1;
+        private TableLayoutPanel tableLayoutPanel1;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private Label label9;
+        private Label label10;
+    }
+}
