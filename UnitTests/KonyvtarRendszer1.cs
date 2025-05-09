70c8b9f8563382f20a49ab75207ec6bc22305753
AlbertPJ33 <pala.albert.jozsef@gmail.com>
2025-05-09 17:48:36 +0200

    Create KonyvtarRendszer1.cs

---
+++ UnitTests/KonyvtarRendszer1.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Moq;
+using Konyvtar_Rendszer_Kezeles;
+
+namespace UnitTests
+{
+    public class KonyvtarRendszerTests1
+    {
+        [Fact]
+        public void HozzaadObserver_ObserverHozzaadva()
+        {
+            var konyvtarRendszer = new KonyvtarRendszer();
+            var observer = new Mock<IObserver>().Object;
+
+            konyvtarRendszer.HozzaadObserver(observer);
+            Assert.Contains(observer, konyvtarRendszer.GetObservers());
+        }
+
+        [Fact]
+        public void TorolObserver_ObserverEltavolitva()
+        {
+
+            var konyvtarRendszer = new KonyvtarRendszer();
+            var observer = new Mock<IObserver>().Object;
+            konyvtarRendszer.HozzaadObserver(observer);
+
+            konyvtarRendszer.TorolObserver(observer);
+
+            Assert.DoesNotContain(observer, konyvtarRendszer.GetObservers());
+        }
+
+        [Fact]
+        public void AddKonyv_KonyvHozzaadvaEsErtesitve()
+        {
+            var konyvtarRendszer = new KonyvtarRendszer();
+            var konyv = new KonyvekHozzaadasa { ID = 1, KonyvCime = "Harry Potter" };
+            var observerMock = new Mock<IObserver>();
+            konyvtarRendszer.HozzaadObserver(observerMock.Object);
+
+            konyvtarRendszer.AddKonyv(konyv);
+
+            Assert.Contains(konyv, konyvtarRendszer.GetKonyvek());
+            observerMock.Verify(o => o.Update("Új könyv hozzáadva: Harry Potter"), Times.Once);
+        }
+
+        [Fact]
+        public void RemoveKonyv_KonyvEltavolitvaEsErtesitve()
+        {
+            var konyvtarRendszer = new KonyvtarRendszer();
+            var konyv = new KonyvekHozzaadasa { ID = 1, KonyvCime = "Harry Potter" };
+            var observerMock = new Mock<IObserver>();
+            konyvtarRendszer.HozzaadObserver(observerMock.Object);
+            konyvtarRendszer.AddKonyv(konyv);
+
+            konyvtarRendszer.RemoveKonyv(1);
+
+            Assert.DoesNotContain(konyv, konyvtarRendszer.GetKonyvek());
+            observerMock.Verify(o => o.Update("Könyv eltávolítva ID: 1"), Times.Once);
+        }
