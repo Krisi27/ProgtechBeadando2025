ebc32140fba06843b06afc220f463402435b8efb
GaborToth40 <tothgabor658@gmail.com>
2025-05-09 17:53:04 +0200

    Update KonyvtarRendszer2.cs

--- UnitTests/KonyvtarRendszer2.cs
+++ UnitTests/KonyvtarRendszer2.cs
@@ -33,3 +33,45 @@ namespace UnitTests
             Assert.Contains(konyv, konyvtarRendszer.GetVisszahozottKonyvek());
             observerMock.Verify(o => o.Update("Könyv visszavétele:" + KonyvCime = "Harry Potter"), Times.Once);
         }
+        
+        [Fact]
+        public void RemoveVisszahozottKonyv()
+        {
+            var konyvtarRendszer = new KonyvtarRendszer();
+            var konyv = new VisszahozottKonyvek { ID = 1, KonyvCime = "Harry Potter" };
+            var observerMock = new Mock<IObserver>();
+            konyvtarRendszer.HozzaadObserver(observerMock.Object);
+            konyvtarRendszer.VisszaKonyv(konyv);
+            konyvtarRendszer.VisszahozottKonyvTorlese(konyv);
+
+            Assert.DoesNotContain(konyv, konyvtarRendszer.GetVisszahozottKonyvek());
+            observerMock.Verify(o => o.Update("Visszahozott könyv eltávolítva ID: 1"), Times.Once);
+        }
+
+        [Fact]
+        public void GetKonyvek()
+        {
+            ar konyvtarRendszer = new KonyvtarRendszer();
+            var konyv1 = new KonyvekHozzaadasa { ID = 1, KonyvCime = "Harry Potter" };
+            var konyv2 = new KonyvekHozzaadasa { ID = 2, KonyvCime = "A kis herceg" };
+
+            konyvtarRendszer.AddKonyv(konyv1);
+            konyvtarRendszer.AddKonyv(konyv2);
+            var konyvek = konyvtarRendszer.GetKonyvek();
+
+            Assert.Contains(konyvek, k => k.ID == konyv1.ID);
+            Assert.Contains(konyvek, k => k.ID == konyv2.ID);
+        }
+
+        [Fact]
+        public void GetElerhetoKonyvek()
+        {
+            var konyvtarRendszer = new KonyvtarRendszer();
+            var konyv = new KonyvekHozzaadasa { ID = 1, KonyvCime = "Harry Potter", Statusz = "Elérhető" };
+            konyvtarRendszer.AddKonyv(konyv);
+            var elerhetoKonyvek = konyvtarRendszer.GetElerhetoKonyvek();
+
+            Assert.Contains(elerhetoKonyvek, k => k.KonyvCime == konyv.KonyvCime && k.Statusz == konyv.Statusz);
+        }
+    }
+}
